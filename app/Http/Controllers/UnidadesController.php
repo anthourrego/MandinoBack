16b224a8cbc1c9c2d50009af38427a67e2873d2a
AlejoHurtatis14 <neideralejandro.h@gmail.com>
2022-01-23 21:29:45 -0500

    Se valida progresos cursos y unidades.

--- app/Http/Controllers/UnidadesController.php
+++ app/Http/Controllers/UnidadesController.php
@@ -316,12 +316,20 @@ class UnidadesController extends Controller
     public function listaUnidadesProgreso($idCurso, $idUser) {
 
         $lecciones = DB::table('lecciones_unidades')
-            ->selectRaw('
-                COUNT(*) AS cantLecciones,
-                IF(lecciones_progreso_usuarios.fecha_completado, COUNT(*), 0) AS cantLeccionesCompletas,
+            ->selectRaw('IF(
+                COUNT(*) = (
+                    IF(lecciones_progreso_usuarios.fecha_completado, COUNT(*), 0)
+                ), 1, 0) AS Completa,
+                (
+                    (
+                        IF(
+                            COUNT(lecciones_progreso_usuarios.fecha_completado) IS NULL, 0, COUNT(lecciones_progreso_usuarios.fecha_completado)
+                        ) * 100
+                    ) / COUNT(*)
+                ) AS progresoActual,
                 lecciones_unidades.fk_unidad
             ')
-            ->join('lecciones_progreso_usuarios', 'lecciones_unidades.fk_leccion', '=', 'lecciones_progreso_usuarios.fk_leccion')
+            ->leftJoin('lecciones_progreso_usuarios', 'lecciones_unidades.fk_leccion', '=', 'lecciones_progreso_usuarios.fk_leccion')
             ->where('lecciones_progreso_usuarios.fk_user', $idUser)
             ->groupBy('lecciones_unidades.fk_unidad');
 
@@ -340,16 +348,10 @@ class UnidadesController extends Controller
                 "unidades.id AS unidadId",
                 "unidades.nombre AS nombre",
                 "unidades_cursos.fk_unidad_dependencia AS unidadDependencia", 
-                "unidades.descripcion AS descripcion"
-            )
-            ->selectRaw(
-                "IF(lecciones.cantLeccionesCompletas IS NULL, 0, lecciones.cantLeccionesCompletas) AS cantLeccionesCompletasDepende,
-                IF(lecciones.cantLecciones IS NULL, 0, lecciones.cantLecciones) AS cantLeccionesDepende,
-                (
-                    (
-                        IF(lecciones2.cantLeccionesCompletas IS NULL, 0, lecciones2.cantLeccionesCompletas) * 100
-                    ) / IF(lecciones2.cantLecciones IS NULL, 0, lecciones2.cantLecciones)
-                ) AS progresoActual"
+                "unidades.descripcion AS descripcion",
+                "lecciones2.*",
+                "lecciones.Completa AS completaDepende",
+                "lecciones.progresoActual AS progresoActualDepende"
             )
             ->orderBy('unidades_cursos.orden','asc');
         
